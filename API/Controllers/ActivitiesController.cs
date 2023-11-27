50ea0d34529bfaf0e61ad6ed8cd81d812263ad80
tahakinci <tahakinci29@gmail.com>
2023-11-27 16:27:28 +0300

    ende of section - 4 (fully)

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,10 +1,7 @@
 
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
